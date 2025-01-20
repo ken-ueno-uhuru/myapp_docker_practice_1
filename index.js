effa97b4285f74829ac81734844d4edb6ae14436
Asuka Toyoshima <asuka.toyoshima@di-v.co.jp>
2025-01-20 16:29:45 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get( '/' , function(req,res){
+  // 変数sampleに"こんにちは"という文字列を代入する
+  let sample = "こんにちは"
+  // 変数sampleに代入された値をブラウザに表示する
+  res.send(`<p>TestText${sample}</p>`);
+});
+
+app.listen(3000, function(){
+  console.log("Listening on localhost port 3000")
+});
