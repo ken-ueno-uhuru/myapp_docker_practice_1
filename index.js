52552c5377815f9bca40e73f4e3b4c40c279fc12
Asuka Toyoshima <asuka.toyoshima@di-v.co.jp>
2025-01-20 17:39:15 +0900

    パス修正

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get( '/' , function(req,res){
+app.get( '/hello' , function(req,res){
   // 変数sampleに"こんにちは"という文字列を代入する
   let sample = "こんにちは"
   // 変数sampleに代入された値をブラウザに表示する
